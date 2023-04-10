3db7c3abbb867b77c94c7d18d2bde074f36ea465
Anurag <anurag.official626@gmail.com>
2023-04-10 22:46:28 +0530

    Chapter 6 completed

--- src/CWH_29_Ch6_PS.java
+++ src/CWH_29_Ch6_PS.java
@@ -5,17 +5,70 @@ public class CWH_29_Ch6_PS {
 
 
         //Question 1
-
-        float [] floats = {5.6f, 8.95f, 5.65f, 87.2f, 45.63f};
+/*         float [] floats = {5.6f, 8.95f, 5.65f, 87.2f, 45.63f};
         float sum = 0;
         for(int i = 0; i < floats.length; i++){
             sum += floats[i];
         }
         System.out.println(sum);
+*/
 
-        int given_int = 45;
+            //Question 2
+     /*
+        int given_int = 49;
         int [] array = {56,58,47,49,45};
-        Arrays.stream(array).findFirst()
+        boolean isInArray = false;
+        for (int element:array){
+            if (given_int == element){
+                isInArray = true;
+                break;
+            }
+        }
+        if(isInArray){
+            System.out.println("The Value is present in Array.");
+        }
+        else{
+            System.out.println("The Value is not present in Array.");
+        }
+*/
+
+
+        // Question 3
+/*        float [] physicsMarks = {56, 89, 59, 87, 45};
+        float sum = 0;
+        for(float marks:physicsMarks){
+            sum = sum+marks;
+        }
+        float average = (sum/physicsMarks.length);
+        System.out.printf("%f is the average of all Physics marks obtained by students", average);
+*/
+
+        // Question 6
+
+        int [] array = {5, 4, 8, 2, 1, 3};
+        int i = 0;
+        for(int numbers:array){
+            if(numbers>i){
+                i=numbers;
+            }
+        }
+        System.out.printf("%d is the greatest element in given array.", i);
+        System.out.println("");
+
+
+
+        // Question 6
+
+        int [] array2 = {5, 4, 8, 2, 1, 3};
+        int j = 0;
+        for(int element:array2){
+            if(element<j){
+                j=element;
+            }
+        }
+        System.out.printf("%d is the smallest element in given array.", j);
+
+
 
     }
 }
